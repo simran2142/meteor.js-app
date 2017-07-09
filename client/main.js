5393e0db8011c9db9d25b4d1ed3c47a4f552fc7b
Simranjit Singh <simranjitsingh214@yahoo.com>
2017-07-09 16:17:09 -0400

    first commit

---
+++ client/main.js
@@ -0,0 +1,61 @@
+import { Template } from 'meteor/templating';
+import { Notes } from '../lib/collection.js';
+import { Accounts } from 'meteor/accounts-base';
+
+
+// Accounts config
+Accounts.ui.config({
+	passwordSignupFields: 'USERNAME_ONLY'
+})
+
+import './main.html';
+
+Template.body.helpers({
+	// notes: [
+	// 	{text: 'My Note 1'},
+	// 	{text: 'My Note 2'},
+	// 	{text: 'My Note 3'}
+	// ]
+
+	notes(){
+		return Notes.find({});
+	}
+	});
+
+Template.add.events({
+	'submit .add-form': function(){
+		event.preventDefault();
+
+		// Get input value
+		const target = event.target;
+		const text = target.text.value;
+
+		// Insert note into collection
+
+		// Notes.insert({
+		// 	// text: text,
+		// 	text,
+		// 	createdAt: new Date(),
+		// 	owner: Meteor.userId(),
+		// 	username: Meteor.user().username
+		// });
+		Meteor.call('notes.insert', text);
+		// Clear form
+		target.text.value = "";
+
+		//Close the modal
+		  $('#addModal').modal('close');
+
+
+		return false;
+	}
+})
+
+Template.note.events({
+	'click .delete-note': function(){
+		//Notes.remove(this._id);
+		Meteor.call('notes.remove', this);
+		return false;
+	}
+})
+
